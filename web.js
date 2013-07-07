d7f8aac1d3b7606beb07bb77c4b7188c01a56f18
interwebcoding <brendon@interwebcoding.com>
2013-07-07 07:56:28 +0000

    buffer add

--- web.js
+++ web.js
@@ -2,8 +2,8 @@ var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buf = new Buffer(1024);
-var message = '';
+// var buf = new Buffer(1024);
+// var message = '';
 // buf = fs.readFileSync('index.html','utf8');
 // message = buf.toString('utf8')
 
